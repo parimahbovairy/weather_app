bf03ccba6e2af754aa65bd3a2b46007fed07e041
Parimah Bovairy <111119618+parimahbovairy@users.noreply.github.com>
2026-09-05 12:11:41 +0330

    Update app.js
    Update app.js: Change API From Accuweather to openweathermap

--- weather_app/scripts/app.js
+++ weather_app/scripts/app.js
@@ -5,51 +5,42 @@ const time = document.querySelector('img.time');
 const icon = document.querySelector('.icon img');
 
 const updateUI = (data) => {
-  // ✨🎃 destructure properties
   const { cityDets, weather } = data;
 
-  // ✨🎃 update details template
   details.innerHTML = `
-    <h5 class="my-3">${cityDets.EnglishName}</h5>
-    <div class="my-3">${weather.WeatherText}</div>
+    <h5 class="my-3">${cityDets.name}</h5>
+    <div class="my-3">${weather.weather[0].description}</div>
     <div class="display-4 my-4">
-      <span>${weather.Temperature.Metric.Value}</span>
+      <span>${Math.round(weather.main.temp)}</span>
       <span>&deg;C</span>
     </div>
   `;
 
-  // ✨🎃 update the night/day & icon images
-  const iconSrc = `img/icons/${weather.WeatherIcon}.svg`;
+  const iconSrc = `https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`;
   icon.setAttribute('src', iconSrc);
-  
-  const timeSrc = weather.IsDayTime ? 'img/day.svg' : 'img/night.svg';
+
+  const isDayTime = weather.weather[0].icon.endsWith('d');
+  const timeSrc = isDayTime ? 'img/day.svg' : 'img/night.svg';
   time.setAttribute('src', timeSrc);
 
-  // ✨🎃 remove the d-none class if present
-  if(card.classList.contains('d-none')){
+  if (card.classList.contains('d-none')) {
     card.classList.remove('d-none');
   }
 };
 
 const updateCity = async (city) => {
-
   const cityDets = await getCity(city);
-  const weather = await getWeather(cityDets.Key);
+  const weather = await getWeather(cityDets.lat, cityDets.lon);
   return { cityDets, weather };
-
 };
 
 cityForm.addEventListener('submit', e => {
-  // ✨🎃 prevent default action
   e.preventDefault();
-  
-  // ✨🎃 get city value
+
   const city = cityForm.city.value.trim();
   cityForm.reset();
 
-  // ✨🎃 update the ui with new city
   updateCity(city)
     .then(data => updateUI(data))
     .catch(err => console.log(err));
 });
-
